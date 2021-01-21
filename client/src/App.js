c222a1168e67d527b20658fda20fa10a18bc1115
LisaAAAlisa <alisaamacedo@gmail.com>
2021-01-21 16:23:59 -0600

    new app migrated without auth0 for sake of functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,55 +1,33 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Container from "./components/Container"
 // import Books from "./pages/Books";
 // import Detail from "./pages/Detail";
-import NoMatch from "./pages/NoMatch";
+// import NoMatch from "./pages/NoMatch";
 // import Nav from "./components/Nav";
-import NavBar from "./components/NavBar";
+// import NavBar from "./components/Auth0NavBar";
 //import Loading from "./components/Loading";
 // import ExternalApi from "./views/ExternalApi";
 import { useAuth0 } from "@auth0/auth0-react";
 // import { Container } from "reactstrap";
 
 // fontawesome
-import initFontAwesome from "./utils/initFontAwesome";
-initFontAwesome();
+// import initFontAwesome from "./utils/initFontAwesome";
+// initFontAwesome();
 
 function App() {
 
-     const {
-      isLoading,
-      isAuthenticated,
-      error,
-      user,
-      loginWithRedirect,
-      logout,
-    } = useAuth0();
-
-    if (isLoading) {
-      return <div>Loading...</div>;
-    }
-    if (error) {
-      return <div>Oops... {error.message}</div>;
-    }
-
-  // if (isLoading) {
-  //   return <Loading />;
-  // }
-
-  if (isAuthenticated) {
+ 
+ 
     return (
     <Router>
       <div>
-      Hello {user.name}{' '}
-        <button onClick={() => logout({ returnTo: window.location.origin })}>
-          Log out
-        </button>
+        <Route exact path="/" component={Container} />
       </div>
     </Router>
   );
-     } else {
-    return <button onClick={loginWithRedirect}>Log in</button>
-  }
+    
+  
 }
 
 export default App;
