100bcfc425a3c92c9afab1f83265bd6f25f015c3
LisaAAAlisa <alisaamacedo@gmail.com>
2021-01-20 22:23:55 -0600

    gitignore

--- client/src/App.js
+++ client/src/App.js
@@ -5,10 +5,10 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import NoMatch from "./pages/NoMatch";
 // import Nav from "./components/Nav";
 import NavBar from "./components/NavBar";
-// import Loading from "./components/Loading";
+import Loading from "./components/Loading";
 // import ExternalApi from "./views/ExternalApi";
 import { useAuth0 } from "@auth0/auth0-react";
-import { Container } from "reactstrap";
+// import { Container } from "reactstrap";
 
 // fontawesome
 import initFontAwesome from "./utils/initFontAwesome";
@@ -26,9 +26,9 @@ function App() {
       logout,
     } = useAuth0();
 
-    // if (isLoading) {
-    //   return <div>Loading...</div>;
-    // }
+    if (isLoading) {
+      return <div>Loading...</div>;
+    }
     if (error) {
       return <div>Oops... {error.message}</div>;
     }
